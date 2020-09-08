9980c1e818aac8b9a2e169f1e389180a30b29c00
MadaZZ <sparshranaazz@gmail.com>
2020-09-08 16:41:44 +0530

    Added closure1.js

---
+++ 3 Closure and Prototype Inheritance/4 closure1.js
@@ -0,0 +1,35 @@
+/**
+ * Lexical scope of c() is different than a() and b()
+ * Even after a() and b() are popped out of the callstack, variables are preserved
+ */ 
+function a() {
+    let grandpa = 'grandpa'
+    return function b() {
+      let father = 'father'
+      return function c() {
+        let son = 'son'
+        return `${grandpa} > ${father} > ${son}`
+      }
+    }
+  }
+  
+  console.log(a()()());
+  
+  //closures and higher order function
+  function boo(string) {
+    return function(name) {
+      return function(name2) {
+        console.log(`hi ${name2}`)
+      }
+    }
+  }
+  
+  const boo2 = (string) => (name) => (name2) => console.log(`hi ${name2}`)
+  
+  boo('hi')('john')('kara');
+  
+  // AHH! HOW DOES IT REMEMBER THIS 5 years from now?
+  booString = boo2('sing');
+  booStringName = booString('John');
+  booStringNameName2 = booStringName('kara')
+  
