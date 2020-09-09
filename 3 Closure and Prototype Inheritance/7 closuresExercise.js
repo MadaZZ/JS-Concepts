fbd99bbff4ddcaf100b2fc0d1ab26149125011c2
MadaZZ <sparshranaazz@gmail.com>
2020-09-09 15:24:11 +0530

    Added initial problem to closuresExercise.js

--- 3 Closure and Prototype Inheritance/7 closuresExercise.js
+++ 3 Closure and Prototype Inheritance/7 closuresExercise.js
@@ -1,5 +1,20 @@
 // How do we control the view from getting loaded more than once even if the function is called multiple times?
 
+// Initial Problem
+let view;
+function initialize() {
+      view = '🏔';
+      console.log('view has been set!')
+}
+
+initialize();
+initialize();
+initialize();
+
+console.log(view)
+
+
+// Solution code
 let view;
 const viewInit = (function () {
     let initNum = 1; // The external calling function does not have access to this variable
