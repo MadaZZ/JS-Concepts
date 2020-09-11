4300a259a7d43f7363bb661ec6ad47bddcdcadd8
MadaZZ <sparshranaazz@gmail.com>
2020-09-11 23:28:42 +0530

    Added fourPillarsOfOOP.js
    This file is open for contribution, will accept Code that explains all
    the four pillars with code and proper comments.

---
+++ 5 Object Oriented Programming/10 fourPillarsOfOOP.js
@@ -0,0 +1,8 @@
+/** FOUR PILLARS OF OBJECT ORIENTED PROGRAMMING
+ * - ENCAPSULATION
+ * - ABSTRACTION
+ * - INHERITANCE
+ * - POLYMORPHISM
+ * To add code for each feature - open for contributions
+*/
+
