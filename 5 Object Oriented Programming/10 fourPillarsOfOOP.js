449335e18d47b87b72ab461bcd64d2c2e01ccb62
MadaZZ <sparshranaazz@gmail.com>
2020-09-11 23:29:42 +0530

    Modified fourPillarsOfOOP.js
    Added explanatory comments.

--- 5 Object Oriented Programming/10 fourPillarsOfOOP.js
+++ 5 Object Oriented Programming/10 fourPillarsOfOOP.js
@@ -3,6 +3,8 @@
  * - ABSTRACTION
  * - INHERITANCE
  * - POLYMORPHISM
+ * 
+ * 
  * To add code for each feature - open for contributions
+ * Will accept Code that explains all the four pillars with code and proper comments.
 */
-
