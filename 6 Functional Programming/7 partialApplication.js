36a1163316ac06a09b855345ab539924da13f702
MadaZZ <sparshranaazz@gmail.com>
2020-09-12 18:54:47 +0530

    Modified partialApplication.js
    Added more comments to explain

--- 6 Functional Programming/7 partialApplication.js
+++ 6 Functional Programming/7 partialApplication.js
@@ -8,10 +8,10 @@
 const multiply = (a, b, c) => a * b * c
 const partialMultiplyBy5 = multiply.bind(null, 5)
 
-console.log(partialMultiplyBy5(10, 20))
+console.log(partialMultiplyBy5(10, 20)) // All the arguments in the second call
 
 //currying
 const multiplyCurrying = (a) => (b) => (c) => a * b * c
 const multiplyCurryingby5 = multiplyCurrying(5)
 
-console.log(multiplyCurryingby5(10)(20))
+console.log(multiplyCurryingby5(10)(20)) // One argument at a time
