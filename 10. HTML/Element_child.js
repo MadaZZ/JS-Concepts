9dde7ee12fffaa49867153444e00cec31b7be4d8
Vishnu <61042678+MEME-MAN234@users.noreply.github.com>
2020-09-30 14:33:31 +0530

    Added Element_child

---
+++ 10. HTML/Element_child.js
@@ -0,0 +1,6 @@
+//Manipulating HTML tag's child(tag oprtions)
+const link = document.querySelector("a"); //Selecting the <a></a> tag
+link.textContent = "Here"; //Changing the text inside the tag
+link.href = "https://developer.mozilla.org"; //Changing the href of a tag
+//End result-
+// <a href="https://www.youtube.com/">Here</a>
