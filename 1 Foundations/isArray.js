012c34c6b3a3697cf621e582c3d7fdaf0a69096b
Saurabh Kanswal <saurabhkanswal8101@gmail.com>
2020-10-05 22:56:52 +0530

    Update isArray.js

--- 1 Foundations/isArray.js
+++ 1 Foundations/isArray.js
@@ -1,15 +1,17 @@
-//Array.isArray()
+//Topic: Array.isArray()
 // The Array.isArray() method determines whether the passed value is an Array or not.
 
-// Resource: http://web.mit.edu/jwalden/www/isArray.html
-
+// Resource:
+//         1- http://web.mit.edu/jwalden/www/isArray.html
 // syntax: Array.isArray(value)
-
 // Parameters:
-//            value means the value to be checked
+//           1- value means the value to be checked
 
-(function () {
-    console.log(Array.isArray(arguments))
-})()
+
 
 // Return value: true if given parameter is an array otherwise it return false
+(function () {
+    console.log(Array.isArray(arguments)) //checking that arguments is array or not.
+})()
+
+
