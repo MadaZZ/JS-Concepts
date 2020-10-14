ab01ee4d9574e6df669a646dad8e58e88ceb4f82
sabiha22 <54326183+sabiha22@users.noreply.github.com>
2020-10-14 17:38:23 +0530

    Update 10 fourPillarsOfOOP.js

--- 5 Object Oriented Programming/10 fourPillarsOfOOP.js
+++ 5 Object Oriented Programming/10 fourPillarsOfOOP.js
@@ -1,8 +1,30 @@
 /** FOUR PILLARS OF OBJECT ORIENTED PROGRAMMING
  * - ENCAPSULATION
  * - ABSTRACTION
- * - INHERITANCE
- * - POLYMORPHISM - Polymorphism provides an ability to call the same method on different JavaScript objects */
+ /* INHERITANCE - The JavaScript inheritance is a mechanism that allows us to create new classes on the basis of already existing classes. 
+                   It provides flexibility to the child class to reuse the methods and variables of a parent class.
+                   The JavaScript extends keyword is used to create a child class on the basis of a parent class. 
+                   It facilitates child class to acquire all the properties and behavior of its parent class.*/
+class Shoes  
+{  
+  constructor()  
+  {  
+    this.company="Adidas";  
+  }  
+}  
+class Product extends Shoes {  
+  constructor(name,price) {  
+   super();  
+    this.name=name;  
+    this.price=price;  
+  }   
+}  
+var s = new Product("Sneakers","3000");  
+console.log(s.company+" "+s.name+" "+s.price);
+
+
+
+/* POLYMORPHISM - Polymorphism provides an ability to call the same method on different JavaScript objects */
 class A  
   {  
      display()  
