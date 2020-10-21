edf00a9974eff45d5fccebeec5cd7898a7c8fc5c
sabiha22 <54326183+sabiha22@users.noreply.github.com>
2020-10-21 11:21:18 +0530

    Update 10 fourPillarsOfOOP.js

--- 5 Object Oriented Programming/10 fourPillarsOfOOP.js
+++ 5 Object Oriented Programming/10 fourPillarsOfOOP.js
@@ -1,17 +1,25 @@
-/* RESOURCES
-   https://www.javatpoint.com/javascript-oops-abstraction
-   https://www.javatpoint.com/javascript-oops-polymorphism
-   https://www.javatpoint.com/javascript-oops-inheritance
-   https://www.javatpoint.com/javascript-oops-encapsulation
-   https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS */
-   
+/* *FOUR PILLARS OF OBJECT ORIENTED PROGRAMMING
+   *ENCAPSULATION- The JavaScript Encapsulation is a process of binding the data with the functions acting on that data. It allows us to control the data 
+                   and validate it. To achieve an encapsulation in JavaScript we use var keyword to make data members private and use setter methods 
+                   to set the data and getter methods to get that data.
+   * ABSTRACTION - JavaScript abstraction is the process of hiding the implementation details and displaying only the functionality to all the users. 
+                   JavaScript Abstraction ignores the unnecessary details and displays only the necessary ones.
+   * INHERITANCE - The JavaScript inheritance is a mechanism that allows us to create new classes on the basis of already existing classes. 
+                   It provides flexibility to the child class to reuse the methods and variables of a parent class.
+                   The JavaScript extends keyword is used to create a child class on the basis of a parent class. 
+                   It facilitates child class to acquire all the properties and behavior of its parent class.
+   * POLYMORPHISM - Polymorphism provides an ability to call the same method on different JavaScript objects
+   *                  
+   * Resources:
+   * - https://www.javatpoint.com/javascript-oops-abstraction
+   * - https://www.javatpoint.com/javascript-oops-polymorphism
+   * - https://www.javatpoint.com/javascript-oops-inheritance
+   * - https://www.javatpoint.com/javascript-oops-encapsulation
+   * - https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
+*/
+  
 
-// FOUR PILLARS OF OBJECT ORIENTED PROGRAMMING
-
-/* - ENCAPSULATION- The JavaScript Encapsulation is a process of binding the data with the functions acting on that data. It allows us to control the data 
-                     and validate it. To achieve an encapsulation in JavaScript we use var keyword to make data members private and use setter methods 
-                     to set the data and getter methods to get that data. */
-
+//ENCAPSULATION
 class Student  
 {  
     constructor()  
@@ -41,11 +49,8 @@ stud.setName("Alicia");
 stud.setMarks(95);  
 console.log(stud.getName()+" "+stud.getMarks());  // prints name and marks of the student
 
-
-
- /* ABSTRACTION - JavaScript abstraction is the process of hiding the implementation details and displaying only the functionality to all the users. 
-                  JavaScript Abstraction ignores the unnecessary details and displays only the necessary ones. */
-
+ 
+//ABSTRACTION
 class ImplementAbstraction {
  // method to set values of internal members
   set(x, y) {
@@ -64,10 +69,7 @@ obj.display();      // a = 10      b = 20
 
 
 
- /* INHERITANCE - The JavaScript inheritance is a mechanism that allows us to create new classes on the basis of already existing classes. 
-                   It provides flexibility to the child class to reuse the methods and variables of a parent class.
-                   The JavaScript extends keyword is used to create a child class on the basis of a parent class. 
-                   It facilitates child class to acquire all the properties and behavior of its parent class.*/
+//INHERITANCE
 class Shoes  
 {  
   constructor()  
@@ -86,8 +88,7 @@ var s = new Product("Sneakers","3000");            // constructor of sub-class i
 console.log(s.company+" "+s.name+" "+s.price);     // prints company after inheriting it from parent class 
 
 
-
-/* POLYMORPHISM - Polymorphism provides an ability to call the same method on different JavaScript objects */
+//POLYMORPHISM
 class A  
   {  
      display()  
